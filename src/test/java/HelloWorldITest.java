a2f40c41172899518e7b85c9b513f6d7634762ac
Sia <saflaki@bol.com>
2022-07-27 11:52:31 +0200

    REVERT

--- src/test/java/HelloWorldITest.java
+++ src/test/java/HelloWorldITest.java
@@ -7,39 +7,4 @@ public class HelloWorldITest {
   public void test() {
     assertTrue(true);
   }
-
-  @Test
-  public void test1() {
-    assertTrue(true);
-  }
-
-  @Test
-  public void test2() {
-    assertTrue(true);
-  }
-
-  @Test
-  public void test3() {
-    assertTrue(true);
-  }
-
-  @Test
-  public void test4() {
-    assertTrue(true);
-  }
-
-  @Test
-  public void test5() {
-    assertTrue(true);
-  }
-
-  @Test
-  public void test6() {
-    assertTrue(true);
-  }
-
-  @Test
-  public void test7() {
-    assertTrue(true);
-  }
 }
